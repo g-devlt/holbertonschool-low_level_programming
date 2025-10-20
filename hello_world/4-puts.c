42856c5f1e14d374caa32e2af93273150b9503a6
g-devlt <gabriel.devasselot.de.regne@gmail.com>
2025-10-20 12:18:07 +0000

    4-puts is now betty compliant

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
+/**
+ * main - Prints "\"Programming is like building a multilingual puzzle" to stdout
+ * Return: Returns 0 on success 
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
