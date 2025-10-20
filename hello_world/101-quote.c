92dadb24dadf92733df984071bf6fa8a83e98021
g-devlt <gabriel.devasselot.de.regne@gmail.com>
2025-10-20 13:37:25 +0000

    Fixed 101-quote.c

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <unistd.h>
+#include <string.h>
 
 /**
  * main - Prints to stderr without forbidden funcstions
@@ -9,6 +10,6 @@ int main(void)
 	const char *out =
 		"and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 
-	write(2, out, sizeof(out));
+	write(2, out, strlen(out));
 	return (1);
 }
