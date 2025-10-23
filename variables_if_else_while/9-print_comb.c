61ced5ff090a08eb0ecc22232d88c340957af730
g-devlt <gabriel.devasselot.de.regne@gmail.com>
2025-10-23 07:53:35 +0000

    Added 9-print_comb.c

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Prints every combination of 2 digits
+ * Return: 0 on success
+*/
+int main(void)
+{
+	int a;
+	int b;
+
+	for (a = '0'; a <= '9'; ++a)
+	{
+		for (b = '0'; b <= '9'; ++b)
+		{
+			if (!(a == '0' && b == '0'))
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			putchar(a);
+			putchar(b);
+		}
+	}
+
+	return (0);
+}
