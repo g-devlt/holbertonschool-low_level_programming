21790bc178f7d8da9cf8a16df458fc85aac7cabd
g-devlt <gabriel.devasselot.de.regne@gmail.com>
2025-10-23 11:26:22 +0000

    102-print_comb5.c

---
+++ variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - Prints every distinct combinations of two two-digits numbers
+ * Return: Returns 0 on success
+*/
+int main(void)
+{
+	int a, b, c, d;
+
+	for (a = '0'; a <= '9'; ++a)
+	{
+		for (b = '0'; b <= '9' - 1; ++b)
+		{
+			for (c = a; c <= '9'; ++c)
+			{
+				for (d = b + 1; d <= '9'; ++d)
+				{
+					if (!(a + b == 2 * '0' && c == '0' && d == '1'))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+					putchar(a);
+					putchar(b);
+					putchar(' ');
+					putchar(c);
+					putchar(d);
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
+
