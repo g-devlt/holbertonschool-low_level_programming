8dc9edf9053d0976855dc8c9a3de6c7307c7578d
g-devlt <gabriel.devasselot.de.regne@gmail.com>
2025-10-24 11:02:14 +0000

    Added 3-islower.c

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _islower - Check if an ascii character is lowercase
+ * @c: The character to check
+ * Return - non-zero if c is lowercase, zero if not.
+*/
+int _islower(int c)
+{
+	if (c == -1)
+		return (0);
+	if (c > 255)
+		return (0);
+
+	unsigned char b = (unsigned char)c;
+	
+	if(97 <= b && b <= 122)
+		return (1);
+	return (0);
+}
