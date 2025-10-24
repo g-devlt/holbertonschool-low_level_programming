3b5e7c831fce79184586ead8359baed04e44ff93
g-devlt <gabriel.devasselot.de.regne@gmail.com>
2025-10-24 12:16:52 +0000

    Fixed 5-sign.c doc

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,5 +1,10 @@
 #include "main.h"
 
+/**
+ * print_sign - Prints the sign of an integer or 0
+ * @n: The number to test
+ * Return: 1 if positive, -1 if negative, 0 if zero
+*/
 int print_sign(int n)
 {
 	if (n == 0)
