ec3ba4bc3fc8667177a1c41658603dec263fa453
g-devlt <gabriel.devasselot.de.regne@gmail.com>
2025-10-24 10:48:53 +0000

    Added 2-print_alphabet_x10.c

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -13,5 +13,10 @@
 	 * print_alphabet - Prints the whole alphabet lowercase
 	*/
 	void print_alphabet(void);
+	
+	/**
+	 * print_alphabet_x10 - Prints the alphabet 10 times
+	*/
+	void print_alphabet_x10(void);
 
 #endif
