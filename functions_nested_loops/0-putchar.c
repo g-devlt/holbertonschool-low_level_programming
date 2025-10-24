62e0450e6e253d7618f905d44a401a7903a3acf7
g-devlt <gabriel.devasselot.de.regne@gmail.com>
2025-10-24 07:34:57 +0000

    Added 0-putchar.c

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+extern int _putchar(char c);
+
+
+/**
+ * main - Prints '_putchar\n'
+ * Return: 0 on success
+*/
+int main(void)
+{
+	const char *str;
+	int i;
+
+	str = "_putchar\n";
+	for (i = 9; i < 10; ++i)
+	{
+		_putchar(str[i]);
+	}
+
+	return (0);
+}
+
---
+++ functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+## 0-putchar.c
+Prints "_putchar", no stdio.h
