3e359d7ac4f5115d2b21cc003fb4dbe83a4be866
g-devlt <gabriel.devasselot.de.regne@gmail.com>
2025-10-24 13:34:14 +0000

    Added 10-add.c

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * add - Adds a and b
+ * @a: The first number
+ * @b: The number to add to a
+ * Return: The sum of a and b
+* /
+int add(int a, int b)
+{
+	return (a + b);
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -32,5 +32,7 @@
 	void jack_bauer(void);
 
 	void times_table(void);
+
+	int add(int, int);
 #endif
 
