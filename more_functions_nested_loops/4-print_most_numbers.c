f2668f3b6f12e7a812df3a3a4502b5c735a803be
g-devlt <gabriel.devasselot.de.regne@gmail.com>
2025-10-27 13:02:40 +0000

    Added 4-print_most_numbers.c

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Prints alomost every digit followed by a new line
+*/
+void print_numbers(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; ++c)
+	{
+		if (c == '2' || c == '4')
+			continue;
+		_putchar(c);
+	}
+	_putchar('\n');
+}
