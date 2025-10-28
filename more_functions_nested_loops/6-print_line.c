d8b6c9f44049e8761c56c81566bf801be4913098
g-devlt <gabriel.devasselot.de.regne@gmail.com>
2025-10-28 08:28:31 +0000

    Added 6-print_line.c

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_line - Prints an horizontal line
+ * @n: Number of characters in the line
+*/
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; ++i)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
