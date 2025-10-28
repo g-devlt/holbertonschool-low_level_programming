202c632cb6239b0c46a589d1b747c0928ecbed3a
g-devlt <gabriel.devasselot.de.regne@gmail.com>
2025-10-28 09:44:52 +0000

    Added 9-fizz_buzz.c

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(void)
+{
+	int i;
+	
+
+	for (i = 1; i <= 100; ++i)
+	{
+		if (i > 1)
+			putchar(' ');
+		if (i % 15 == 0)
+			printf("FizzBuzz");
+		else if (i % 5 == 0)
+			printf("Buzz");
+		else if (i % 3 == 0)
+			printf("Fizz");
+		else
+			printf("%d", i);
+	}
+	putchar('\n');
+
+	return 0;
+}
