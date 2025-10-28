15dceaff0dc48c459678367444c786a9f28f66f1
g-devlt <gabriel.devasselot.de.regne@gmail.com>
2025-10-28 13:41:03 +0000

    Added 4-print_rev.c

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strlen - Calculates the length of a cstring
+ * @str: The cstring
+ * Return: The length of the cstring
+*/
+long long _strlen(char *str)
+{
+	int i = 0;
+
+	while (str[i] != 0)
+		++i;
+	return (i);
+}
+
+/**
+ * print_rev - Prints a cstring in reverse order
+ * @s: The cstring to print
+*/
+void print_rev(char *s)
+{
+	long long i = _strlen(s);
+	
+	while (i > 0)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
+
