7246ce5ee29d494369199f0c87338f9d492464cc
g-devlt <gabriel.devasselot.de.regne@gmail.com>
2025-10-29 11:57:15 +0000

    Added 7-puts_half.c

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strlen - Computes the length of a cstring
+ * @str: The cstring to compute
+ * Return: The length of the string
+*/
+int _strlen(char *str)
+{
+	int i = 0;
+
+	while(str[i] != 0)
+		++i;
+	return i;
+}
+
+/**
+ * puts_half - Prints half of the string provided
+ * @str: The cstring to print half of
+*/
+void puts_half(char *str)
+{
+	int len = _strlen(str);
+	int i = len - (len / 2 + len % 2);
+
+	for (; i < len; ++i)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+
+}
