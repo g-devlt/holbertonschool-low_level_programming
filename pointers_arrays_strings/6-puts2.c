d623c93942eaf3a7bdc044d14576d66d5fc668b1
g-devlt <gabriel.devasselot.de.regne@gmail.com>
2025-10-29 09:32:23 +0000

    Added 6-puts2.c

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * puts2 - Prints every other character of a cstring
+ * @str: The cstring to print
+*/
+void puts2(char *str)
+{
+	int i;
+
+	for(i = 0; str[i] != 0; ++i)
+	{
+		if (i % 2 == 1)
+			continue;
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
