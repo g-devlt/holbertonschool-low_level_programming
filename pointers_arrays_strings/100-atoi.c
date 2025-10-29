79d037834d32bcb6fcf5db2df2c446d4b52d19b6
g-devlt <gabriel.devasselot.de.regne@gmail.com>
2025-10-29 12:28:55 +0000

    Added 100-atoi.c

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * _isdigit - Checks if a character is a digit
+ * @c: The character to check
+ * Return: True if the character is a digit, false otherwise
+*/
+bool _isdigit(char c)
+{
+	return (c >= '0' && c <= '9');
+}
+
+/**
+ * -atoi - Converts a cstring to an integer
+ * @s: The cstring to convert
+ * Return: The number extracted from s.
+*/
+int _atoi(char *s)
+{
+	int n = 0;
+	int i;
+	
+	for (i = 0; s[i] != 0; ++i)
+	{	
+		if (!isdigit(s[i]))
+		{
+			if(s[i] == '-')
+				n *= -1;
+			if (i)
+				break;
+			continue;
+		}
+		else
+		{
+			n *= 10;
+			n += s[i] - '0';
+		}
+	}
+	return (n);
+}
