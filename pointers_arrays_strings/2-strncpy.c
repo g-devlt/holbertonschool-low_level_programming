645f248a050575fcdeac02a877448570e9df69a1
g-devlt <gabriel.devasselot.de.regne@gmail.com>
2025-10-30 13:06:33 +0000

    Fixed 2-strncpy.c

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,6 +1,12 @@
 #include "main.h"
 
-/**/
+/**
+ * _strncpy - Copies n bytes from src to dest, pads with null bytes
+ * @dest: The destination buffer
+ * @src: The source cstring
+ * @n: The number of bytes to copy into dest
+ * Return: The destination buffer
+*/
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0;
