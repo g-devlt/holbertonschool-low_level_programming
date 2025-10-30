b2732787d5c83059a3d51f88b9f89d3812bfda7c
g-devlt <gabriel.devasselot.de.regne@gmail.com>
2025-10-30 14:34:15 +0000

    Added 4-rev_array.c

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * reverse_array - Flips an array
+ * @a: The array of length n to flip
+ * @n: The size of the array a.
+*/
+void reverse_array(int *a, int n)
+{
+	int i = 0;
+	int hl =  n / 2;
+	
+	for(; i < hl; ++i)
+	{
+		int *f = a + i;
+		int *s = a + (n - 1) - i;
+
+		int tmp = *f;
+		*f = *s;
+		*s = tmp;
+	}
+}
