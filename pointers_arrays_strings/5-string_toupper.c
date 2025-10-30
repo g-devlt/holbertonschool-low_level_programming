f296566fe386c9238d2362431f667b7a1acd9ace
g-devlt <gabriel.devasselot.de.regne@gmail.com>
2025-10-30 14:50:57 +0000

    Fixed 5-string_toupper.c

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -9,7 +9,7 @@ char *string_toupper(char *str)
 {
 	int i = 0;
 
-	while(str[i] != 0)
+	while (str[i] != 0)
 	{
 		if(str[i] >= 'a' && str[i] <= 'z')
 		{
@@ -17,5 +17,5 @@ char *string_toupper(char *str)
 		}
 		++i;
 	}
-	return str;
+	return (str);
 }
