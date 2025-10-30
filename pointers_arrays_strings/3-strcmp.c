9774f70a4885ec90da971fcb03835973a518325c
g-devlt <gabriel.devasselot.de.regne@gmail.com>
2025-10-30 14:05:52 +0000

    Added doc

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -1,5 +1,12 @@
 #include "main.h"
 
+/**
+ * _strcmp - Compares two cstring
+ * @s1: The first string
+ * @s2: The second string
+ * Return: 0 if equal, else the diff between s1[i] and s2[i]
+ * where i is the index where s1 and s2 are not matching anymore
+*/
 int _strcmp(char *s1, char *s2)
 {
 	int i = 0;
@@ -9,7 +16,7 @@ int _strcmp(char *s1, char *s2)
 		if (s1[i] == s2[i])
 		{
 			if (s1[i] == 0)
-				return 0;
+				return (0);
 		}
 		else
 			return (s1[i] - s2[i]);
