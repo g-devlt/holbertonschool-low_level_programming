af1b67dda9e7e29f84d817ca88f62a77822e87d0
g-devlt <gabriel.devasselot.de.regne@gmail.com>
2025-11-04 14:27:48 +0000

    Fixed 3-factorial.c

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -1,13 +1,16 @@
 #include "main.h"
 
-/**/
+/**
+ * factorial - Calculates the factorial of n
+ * @n: The number to compute
+ * Return: The factorial of n or -1 if n is negative.
+*/
 int factorial(int n)
 {
 	if (n < 0)
 		return (-1);
 	if (n <= 1) /*0 or 1*/
 		return (1);
-	
 
 	return (n * factorial(n - 1));
 }
