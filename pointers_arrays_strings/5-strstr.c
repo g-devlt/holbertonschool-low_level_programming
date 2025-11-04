0b2e684dcd65fd96a1bfd1b4e1ad921c1e944dbe
g-devlt <gabriel.devasselot.de.regne@gmail.com>
2025-11-04 10:26:11 +0000

    Added 5-strstr.c

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -1,6 +1,12 @@
 #include "main.h"
+#include <stddef.h>
 
-/**/
+/**
+ * _strstr - Finds a substring in a cstring
+ * @haystack: The cstring to search
+ * @needle: The substring to find
+ * Return: A pointer to the substring start
+*/
 char *_strstr(char *haystack, char *needle)
 {
 	int i, c;
@@ -8,17 +14,17 @@ char *_strstr(char *haystack, char *needle)
 	for (; *haystack != 0; ++haystack)
 	{
 		c = 0;
-		for(i = 0; needle[i] != 0; ++i)
+		for (i = 0; needle[i] != 0; ++i)
 		{
-			if(haystack[i] == 0)
+			if (haystack[i] == 0)
 				break;
-			if(haystack[i] == needle[i])
+			if (haystack[i] == needle[i])
 				c = 1;
 			else
 				c = 0;
 		}
-		if(c == 1)
+		if (c == 1)
 			return (haystack);
 	}
-	return NULL;
+	return (NULL);
 }
