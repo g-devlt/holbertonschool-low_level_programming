603d21a7170346df0b1f32923555f60dc83670c2
g-devlt <gabriel.devasselot.de.regne@gmail.com>
2025-11-04 08:57:48 +0000

    Fixed 2-strchr.c

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -1,14 +1,20 @@
 #include "main.h"
+#include <stddef.h>
 
-/**/
+/**
+ * _strchr - Finds a char in a cstring
+ * @s: The cstring to search
+ * @c: The character to find
+ * Return: A pointer to the first occurence of the character or NULL
+*/
 char *_strchr(char *s, char c)
 {
 	while (*s != c)
 	{
 		if (*s == 0)
-			return NULL;
+			return (NULL);
 		++s;
 	}
-	return s;
+	return (s);
 }
 
