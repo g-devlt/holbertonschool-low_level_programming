fedde2da8302c56e36f9202881e5a208acd305ee
g-devlt <gabriel.devasselot.de.regne@gmail.com>
2025-11-05 10:42:00 +0000

    Fixed 0-whatsmyname.c

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - Prints the program name
+ * @argc: Argument count
+ * @argv: Argument values
+ * Return: 0 on success
+*/
 int main(int argc, char **argv)
 {
-	printf("%s\n", argc[0]);
-	return 0;
+	printf("%s\n", argv[0]);
+	return (0);
 }
