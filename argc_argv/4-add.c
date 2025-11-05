9dd43590e0dc92a37b4ef961594164f4c3137f66
g-devlt <gabriel.devasselot.de.regne@gmail.com>
2025-11-05 12:43:44 +0000

    Added 4-add.c

---
+++ argc_argv/4-add.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Adds every arguments (must be integers) and prints the sum
+ * @argc: Argument count
+ * @argv: Argument values
+ * Return: 1 on error, 0 on success
+*/
+int main(int argc, char **argv)
+{
+	int sum = 0;
+	int i = 1;
+	char *end;
+
+	for (; i < argc; ++i)
+	{
+		sum += strtol(argv[i], &end, 10);
+		if (end == argv[i])
+		{
+			printf("Error\n");
+			return (1);
+		}
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
