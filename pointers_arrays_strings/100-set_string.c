182b77fee230c924c0c2fe26fae26f34d8a3e05d
g-devlt <gabriel.devasselot.de.regne@gmail.com>
2025-11-06 13:02:22 +0000

    Added 100-set_string.c

---
+++ pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * set_string - Sets string s to string to
+ * @s: The string to replace
+ * @to: The string to replace with
+*/
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
