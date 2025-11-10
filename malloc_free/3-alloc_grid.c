f3f00b7b1c77794a2125f006cd59df235295c1ae
g-devlt <gabriel.devasselot.de.regne@gmail.com>
2025-11-10 14:23:10 +0000

    Added 3-alloc_grid.c

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * alloc_grid - Allocates a 2d array of int
+ * @width: The width of the array
+ * @height: The height of the array
+ * Return: A 2d array of integers
+*/
+int **alloc_grid(int width, int height)
+{
+	int **buf;
+	int i, j;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	buf = malloc(sizeof(int *) * height);
+	if (!buf)
+		return (NULL);
+
+	for (i = 0; i < height; ++i)
+	{
+		buf[i] = malloc(sizeof(int) * width);
+		if (!buf[i])
+			return (NULL);
+		for (j = 0; j < width; ++j)
+		{
+			buf[i][j] = 0;
+		}
+	}
+
+	return (buf);
+}
+
