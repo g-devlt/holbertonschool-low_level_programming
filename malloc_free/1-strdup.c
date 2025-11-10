81b0ed574f633b889c181abe91e375afad1ccc26
g-devlt <gabriel.devasselot.de.regne@gmail.com>
2025-11-10 10:10:37 +0000

    Added 1-strdup.c

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * __strlen - Computes the length of a cstring
+ * @str: The cstring to compute
+ * Return: The length of the cstring
+*/
+int __strlen(char *str)
+{
+	int i = 0;
+
+	while (str[i] != 0)
+		++i;
+
+	return (i);
+}
+
+
+/**
+ * _strdup - Copies a cstring
+ * @str: The cstring to copy
+ * Return: A buffer with a copy of the cstring including the null terminator
+*/
+char *_strdup(char *str)
+{
+	int i;
+	int len;
+	char *buf;
+
+	len = __strlen(str);
+	buf = malloc(len + 1); /*Adjust for null terminator*/
+
+	if (buf == NULL)
+		return (NULL);
+
+	for (i = 0; i < len + 1; ++i)
+	{
+		buf[i] = str[i];
+	}
+	return (buf);
+}
+
