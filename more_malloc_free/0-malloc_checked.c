ba51267b4e5ceaa7f42750e9db76ebc43147f00c
g-devlt <gabriel.devasselot.de.regne@gmail.com>
2025-11-12 09:36:58 +0000

    Betty 0-malloc_checked.c

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -1,11 +1,16 @@
 #include "main.h"
 #include <stdlib.h>
 
-
+/**
+ * malloc_checked - Safe malloc wrapper.
+ * @b: Number of bytes to allocate
+ * Return: The allocated buffer. Exits program on failure
+*/
 void *malloc_checked(unsigned int b)
 {
 	void *buf = malloc(b);
-	if(!buf)
+
+	if (!buf)
 		exit(98);
 	return (buf);
 }
