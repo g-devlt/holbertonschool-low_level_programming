687a07f3ab9be24729ed986cf31df040e6b74c6e
g-devlt <gabriel.devasselot.de.regne@gmail.com>
2025-11-12 10:47:41 +0000

    Added 3-array_range.c

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdlib.h>
+
+int *array_range(int min, int max)
+{
+	unsigned int len;
+	int *buf;
+	int i;
+
+	if (min > max)
+		return (NULL);
+
+	unsigned int len = max - min + 1;
+
+	buf = malloc(sizeof(int) * len);
+
+	for (i = 0; i <= len; ++i)
+	{
+		buf[i] = min + i;
+	}
+
+	return (buf);
+}
