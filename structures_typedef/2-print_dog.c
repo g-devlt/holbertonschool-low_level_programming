75c8d47fe4b787c6a6971cd44061f60a4cc4b352
g-devlt <gabriel.devasselot.de.regne@gmail.com>
2025-11-13 10:51:54 +0000

    Fixed 2-print_dog.c

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -1,5 +1,6 @@
 #include "dog.h"
 #include <stddef.h>
+#include <stdio.h>
 
 /**
  * print_dog - Prints the members of a dog struct
@@ -8,6 +9,6 @@
 void print_dog(struct dog *d)
 {
 	printf("Name: %s", (d->name == NULL) ? "(nil)" : d->name);
-	printf("Age: %.6f");
+	printf("Age: %.6f", d->age);
 	printf("Owner : %s", (d->owner == NULL) ? "(nil)" : d->owner);
 }
