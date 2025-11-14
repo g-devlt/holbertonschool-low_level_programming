135866bf05bf5f61e5952dbf56d7a63a179ba5e7
g-devlt <gabriel.devasselot.de.regne@gmail.com>
2025-11-14 10:33:04 +0000

    Fixed

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -54,16 +54,24 @@ char *_strdup(char *str)
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog = malloc(sizeof(dog_t));
+
 	if (dog == NULL)
 		return (NULL);
 
 	dog->name = _strdup(name);
 	if (dog->name == NULL)
+	{
+		free(dog);
 		return (NULL);
+	}
 
 	dog->owner = _strdup(owner);
 	if (dog->owner == NULL)
+	{
+		free(dog->name);
+		free(dog);
 		return (NULL);
+	}
 
 	dog->age = age;
 
