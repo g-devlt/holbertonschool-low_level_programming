673addb9c78bc0f40195db60fb89ed0b76dad558
g-devlt <gabriel.devasselot.de.regne@gmail.com>
2025-11-14 10:38:24 +0000

    Added 5-free_dog.c

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+
+/**
+ * free_dog - Frees a dog struct
+ * @d: The dog struct to free
+*/
+void free_dog(dog_t *d)
+{
+	if (d->name)
+		free(d->name);
+	if (d->owner)
+		free(d->owner);
+	free(d);
+}
+
