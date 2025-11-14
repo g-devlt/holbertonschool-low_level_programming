a11b32bde7d9555ea48dd79fa5a3cb989aec7ef5
g-devlt <gabriel.devasselot.de.regne@gmail.com>
2025-11-14 10:06:55 +0000

    Fixed 2-print_dog.c

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -10,7 +10,7 @@ void print_dog(struct dog *d)
 {
 	if (!d)
 		return;
-	printf("Name: %s", (d->name == NULL) ? "(nil)" : d->name);
-	printf("Age: %.6f", d->age);
-	printf("Owner : %s", (d->owner == NULL) ? "(nil)" : d->owner);
+	printf("Name: %s\n", (d->name == NULL) ? "(nil)" : d->name);
+	printf("Age: %.6f\n", d->age);
+	printf("Owner : %s\n", (d->owner == NULL) ? "(nil)" : d->owner);
 }
