67321e10ba3f30b4b20a669965ea2c323ffe792f
g-devlt <gabriel.devasselot.de.regne@gmail.com>
2025-11-17 13:00:18 +0000

    Added 0-print_name.c

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - Prints a name using a function pointer
+ * @name: The name to print
+ * @f: The function to use
+*/
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
+
