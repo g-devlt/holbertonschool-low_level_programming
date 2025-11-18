c850c1e2d464cb84003758c6fdd12fb352bf158c
g-devlt <gabriel.devasselot.de.regne@gmail.com>
2025-11-18 12:34:45 +0000

    Added 1-print_numbers.c

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,27 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+ * print_numbers - Print every integer passed as variable arguments
+ * @separator: The separator inbetween every integers
+ * @n: The number of arguments passed (variable arguments)
+ * @...: The numbers to print
+*/
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list vl;
+	unsigned int i;
+
+	va_start(vl, n);
+
+	for (i = 0; i < n; ++i)
+	{
+		if (i && separator)
+		{
+			printf("%s", separator);
+		}
+		printf("%d", va_arg(vl, int));
+	}
+	va_end(vl);
+	printf("\n");
+}
