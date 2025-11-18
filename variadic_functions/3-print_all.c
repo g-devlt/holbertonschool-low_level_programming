c801846f5796079b139f331bfbdba042c53bdf20
g-devlt <gabriel.devasselot.de.regne@gmail.com>
2025-11-18 13:13:18 +0000

    Added 3-print_all.c

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,41 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_all - Prints variadic arguments based on format
+ * @format: Specify how to print Arguments
+ * @...: The arguments to format and print
+*/
+void print_all(const char * const format, ...)
+{
+	unsigned int i;
+	va_list ap;
+
+	va_start(ap, format);
+
+	i = 0;
+	while (format[i] != 0)
+	{
+		switch(format[i])
+		{
+		case 'c':
+			printf("%c", va_arg(ap, int));
+			break;
+		case 'i':
+			printf("%d", va_arg(ap, int));
+			break;
+		case 'f':
+			printf("%f", va_arg(ap, double));
+			break;
+		case 's':
+			printf("%s", va_arg(ap, char *));
+			break;
+		default:
+			break;
+		}
+		++i;
+	}
+	va_end(ap);
+	printf("\n");
+}
