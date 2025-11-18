3c57d665ab4dd1fc1b9c0f0a0b7ccf2160a753e7
g-devlt <gabriel.devasselot.de.regne@gmail.com>
2025-11-18 09:42:20 +0000

    Fix

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -1,16 +1,23 @@
 #include "function_pointers.h"
 
+/**
+ * int_index - Gets the index of the first element fulfilling cmp condition
+ * @array: The array of elements
+ * @size: The size of the array
+ * @cmp: The comparator function
+ * Return: The index of the first element fulfilling cmp condition
+*/
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
 	if (size <= 0)
-		return -1;
+		return (-1);
 
 	for (i = 0; i < size; ++i)
 	{
 		if (cmp(array[i]))
-			return i;
+			return (i);
 	}
 
 	return (-1);
