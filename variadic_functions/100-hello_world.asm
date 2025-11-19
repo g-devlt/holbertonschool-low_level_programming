cc5328ab052a52bee64fb17d9188a6a2bd7f7939
g-devlt <gabriel.devasselot.de.regne@gmail.com>
2025-11-19 10:35:24 +0000

    Added 100-hello_world.asm

---
+++ variadic_functions/100-hello_world.asm
@@ -0,0 +1,21 @@
+[BITS 64]
+
+section .text
+
+global _start
+_start:
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, buf
+	mov rdx, len
+	syscall
+	mov rax, 60
+	mov rdi, 0
+	syscall
+	jmp $
+
+section .data
+
+buf db "Hello, World", 10
+len equ $ - buf
+
