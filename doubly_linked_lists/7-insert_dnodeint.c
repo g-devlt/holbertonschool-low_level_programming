ad24540b93d9a3d98751f1e03878ae707278c91c
g-devlt <gabriel.devasselot.de.regne@gmail.com>
2025-12-03 09:26:06 +0000

    Major fixes

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -1,36 +1,40 @@
 #include "lists.h"
 #include <stdlib.h>
 
+/**
+ * insert_dnodeint_at_index - Inserts a node at the specified index
+ * @h: The head of the list
+ * @idx: The index to insert at
+ * @n: The initialization value of the new node
+*/
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *prev;
 	dlistint_t *next;
 
-	dlistint_t *new = (dlistint_t *)malloc(sizeof(dlistint_t));
-
-	if (new == NULL)
-		return (NULL);
+	dlistint_t *new;
 
 	if (idx == 0)
+		return (add_dnodeint(h, n));
+
+	if (*h == NULL)
+		return (NULL);
+
+	new = (dlistint_t *)malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+
+	prev = *h;
+	for (; idx > 1; --idx)
 	{
-		prev = NULL;
-		next = *h;
-
-		next->prev = new;
+		prev = prev->next;
 	}
-	else
-	{
-		prev = *h;
-		for (; idx > 1; --idx)
-		{
-			prev = prev->next;
-		}
-		next = prev->next;
+	next = prev->next;
 
-		prev->next = new;
+	prev->next = new;
+	if (next)
 		next->prev = new;
-
-	}
 
 	new->prev = prev;
 	new->next = next;
