bb44baeee7924e23dfa67bc1389ee3c79b34a2ce
g-devlt <gabriel.devasselot.de.regne@gmail.com>
2025-12-03 09:05:41 +0000

    Fixed doc

--- doubly_linked_lists/6-sum_dlistint.c
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -1,8 +1,14 @@
 #include "lists.h"
 
+/**
+ * sum_dlistint - Sums a whole dlist
+ * @head: The head of the list
+ * Return: The sum of every int members of each node
+ *
+*/
 int sum_dlistint(dlistint_t *head)
 {
-	if(!head->next)
+	if (!head->next)
 		return (head->n);
 	return (sum_dlistint(head->next) + head->n);
 }
