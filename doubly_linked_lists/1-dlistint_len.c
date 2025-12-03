c9e817fe2aefbe65023a85ffab6e3b90290e72c9
g-devlt <gabriel.devasselot.de.regne@gmail.com>
2025-12-03 08:29:53 +0000

    Added doc to 1-dlistint_len.c

--- doubly_linked_lists/1-dlistint_len.c
+++ doubly_linked_lists/1-dlistint_len.c
@@ -1,5 +1,11 @@
 #include "lists.h"
+#include <stddef.h>
 
+/**
+ * dlistint_len - Computes the size of a doubly linked list
+ * @h: The head of the doubly linked list
+ * Return: The size of the list
+*/
 size_t dlistint_len(const dlistint_t *h)
 {
 	int count = 0;
