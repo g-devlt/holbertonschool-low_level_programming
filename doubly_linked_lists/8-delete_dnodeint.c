85baf40f3ca7b021bfe50367880db70c27822733
g-devlt <gabriel.devasselot.de.regne@gmail.com>
2025-12-03 09:52:01 +0000

    Fixed

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -10,8 +10,9 @@
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 	dlistint_t *node = *head;
-	if(!node)
-		return -1;
+
+	if (!node)
+		return (-1);
 
 	for (; index > 0; --index)
 	{
@@ -19,6 +20,12 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 			return (-1);
 		node = node->next;
 	}
+
+	if (!node)
+		return (-1);
+
+	if (node == *head)
+		*head = node->next;
 
 	if (node->prev)
 		node->prev->next = node->next;
