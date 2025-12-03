c456268e3c8b41a836a79b13f7c0267fe1ea71f3
g-devlt <gabriel.devasselot.de.regne@gmail.com>
2025-12-03 08:27:25 +0000

    Added doc. Fixed typo and added include

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -1,15 +1,21 @@
 #include "lists.h"
+#include <stdio.h>
 
+/**
+ * print_dlistint - Prints a doubly linked list with printf
+ * @h: The head of the doubly linked list
+ * Return: The size of the list
+*/
 size_t print_dlistint(const dlistint_t *h)
 {
 	int count = 0;
 
-	while(h)
+	while (h)
 	{
 		printf("%d\n", h->n);
 
 		h = h->next;
-		count += i;
+		count += 1;
 	}
 
 	return (count);
