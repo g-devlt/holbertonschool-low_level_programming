ea9aaf35fe696312092951425c36f74ba53efdc3
g-devlt <gabriel.devasselot.de.regne@gmail.com>
2025-12-03 09:01:37 +0000

    Added doc and null dereferencing guard

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -1,12 +1,20 @@
 #include "lists.h"
 
+/**
+ * get_dnodeint_at_index - Gets the node at a specified index
+ * @head: The head of the linked list
+ * @index: The index at wich to retrieve the node
+ * Return: A pointer to the retrieved node
+*/
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-	dlistint_t* current = head;
+	dlistint_t *current = head;
 
 	for (; index > 0; --index)
 	{
+		if (!current)
+			return (NULL);
 		current = current->next;
 	}
-	return current;
+	return (current);
 }
